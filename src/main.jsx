847c0b645ad5f8272e4b372bbb4c6acd0f6d3c61
Samiran1234 <samiransetua26@gmail.com>
2024-08-23 16:02:48 +0530

    mutation discussion

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,36 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/Root";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/Root";
 import ErrorPage from "./ErrorPage";
-import Contact from "./routes/Contact";
+import Contact, { loader as contactLoader, } from "./routes/Contact";
+import EditContact, { action as editAction, } from "./routes/Edit";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>,
-    errorElement:<ErrorPage/>,
-    children:[
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
-    ]
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+    ],
   },
-  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
